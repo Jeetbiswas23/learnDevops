061fad0a8ddc361b63e6a36ddf25fbb7b83d47fe
Jeet Biswas <jtbiswass@Jeets-MacBook-Air.local>
2026-09-12 17:57:08 +0530

    new line in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <main style={{ fontFamily: "Arial, sans-serif", maxWidth: 800, margin: "80px auto", padding: 24 }}>
-      <h1>MERN DevOps Learning Project 🚀</h1>
+      <h1>MERN DevOps Learning Projects man 🚀</h1>
       <p>This React frontend will eventually be containerized, tested, deployed and monitored.</p>
       <div style={{ padding: 20, border: "1px solid #ddd", borderRadius: 12, marginTop: 24 }}>
         <strong>Backend response:</strong>
